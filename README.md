aa4f960c76620b6e1d1a42e367b92767262c893d
Ben Pettis <79371825+bpettis@users.noreply.github.com>
2023-06-11 14:03:22 -0500

    Use praw to scrape stickies (#2)
    * Use get-stickies.py from https://github.com/bpettis/reddit-scrape_mods-rules
    
    * cleanup for runnning in isolation
    
    * option to enable/disable cloud logging
    
    * add some timing information
    
    * test cloud logging
    
    * save separate directories for each day
    
    * create requirements.txt
    
    * update readme
    
    * fix recursion in logging function
    
    * explain why some subreddits don't have stickies
    
    * ignore nohup.out

--- README.md
+++ README.md
@@ -24,6 +24,40 @@ I've created this script for my own research purposes, and so I can't necessaril
 
 ## Configuration
 
+### Google Cloud
+
+The `get-stickies.py` script is set up to save the output data into a Google Cloud Storage Bucket. This can be handy for if you are planning on publicizing the data. You'll need to do a bit of setup.
+
+- You will need to create a Google Cloud project (or use an existing one)
+- You will need to create a Storage Bucket (with region/redundancy/privacy settings that fit your needs). 
+- You will need to create a Service Account which has permission to write files into that bucket. 
+- You will need to download a key for that service account in JSON format
+
+The information of the above should be placed in a `.env` file in the root directory:
+
+```
+GCS_BUCKET_NAME='gcs_bucket_name_here'
+GCP_PROJECT='project_name_here'
+GOOGLE_APPLICATION_CREDENTIALS='keys/path/to/gcs-credentials.json'
+```
+
+If you want to disable Google Cloud Storage, just change `use_gcs = True` to `use_gcs = False` toward the top of the script 
+
+#### Cloud Logging
+
+The `get stickies.py` scripy can also log information via Google Cloud logging. If you want to use this option, the service account that you use will need to have permission to write logs as well. 
+
+Set the name of the log by adding a line in the `.env` file in the root directory:
+
+```
+LOG_ID='reddit-blackout-announcements'
+```
+
+If you want to disable Google Cloud Storage, just change `use_cloud_logging = True` to `use_cloud_logging = False` toward the top of the script 
+
+### Google Cloud Authentication
+
+You will need to save your service account keys as a JSON file and place it in a place that the script can find. Set this file path using the `GOOGLE_APPLICATION_CREDENTIALS` environment variable
 
 ### Reddit Authentication:
 
@@ -65,8 +99,40 @@ Set the location and name of the output file at the top of the script
 output = 'output/subreddits.txt'
 ```
 
+### `get-stickies.py`
+
+This is a slightly modified version of the script that I had previously written to preserve other subreddit info: https://github.com/bpettis/reddit-scrape_mods-rules
+
+It will create a CSV file for each of the listed subreddits. Each row of the CSV represents a stickied post. There currently isn't any logic to try and detect which post is the one announcing the blackout. I'm just saving all of them.
+
+Set the list of input subreddits in `input_list = 'output/subreddits.txt'` at the top of the script
+
+That file should be the one created by `list-subreddits.py`
+
+**NOTE*: This script is only going to get the stickied posts from each of the specified subreddits. If the subreddit doesn't have a sticky about their participation in the blackout, it won't be fully represented here. The script will raise a `prawcore.exceptions.NotFound` exception and continue on. This means you'll get some CSV files that look incomplete.
+
 ---
 
 # Data Access
 
-If you're just interested in taking a look at the content that I scraped, I will be making that available: <somewhere, check back later>
+If you're just interested in taking a look at the content that I scraped, I will be making that available:
+
+- List of subreddits: https://storage.googleapis.com/reddit-blackout-announcements/subreddits.txt
+- All stickied posts: https://storage.googleapis.com/reddit-blackout-announcements/
+
+The above URL for all stickied posts will give you an XML file of all contents in the storage bucket. Each `<Contents>` element represents a file:
+
+```
+<Contents>
+    <Key>2023-06-11/stickies/funny.csv</Key>
+    <Generation>1686507430668697</Generation>
+    <MetaGeneration>1</MetaGeneration>
+    <LastModified>2023-06-11T18:17:10.671Z</LastModified>
+    <ETag>"ceab42346825f636b26ec470b417aa8d"</ETag>
+    <Size>2620</Size>
+</Contents>
+```
+
+You can use the `<Key>` to build the URL like this:  `https://storage.googleapis.com/reddit-blackout-announcements` + `<Key>`
+
+For example: [`https://storage.googleapis.com/reddit-blackout-announcements/2023-06-11/stickies/funny.csv`](https://storage.googleapis.com/reddit-blackout-announcements/2023-06-11/stickies/funny.csv)
---
+++ get-stickies.py
@@ -0,0 +1,190 @@
+#! /usr/bin/python3
+
+import praw, prawcore, csv, os
+from datetime import datetime
+from pathlib import Path
+
+from dotenv import load_dotenv, find_dotenv
+from google.cloud import storage
+import google.cloud.logging
+
+# set up some global variables:
+reddit = praw.Reddit("modscraper")
+input_list = 'output/subreddits.txt'
+
+# enable/disable google cloud storage
+use_gcs = True
+
+# enable/disbale Google Cloud Logging
+use_cloud_logging = True
+
+# load environment variables
+load_dotenv(find_dotenv())
+bucket_name = os.environ.get("GCS_BUCKET_NAME")
+project_id = os.environ.get("GCP_PROJECT")
+log_name = os.environ.get("LOG_ID")
+
+
+# Set up Google cloud logging:
+log_client = google.cloud.logging.Client(project=project_id)
+logger = log_client.logger(name=log_name)
+
+def write_log(payload):
+    # takes an input dictionary and writes it to cloud logging - but only after checking if we want to log or not
+    if use_cloud_logging:
+        logger.log_struct(payload)
+    else:
+        return
+
+def get_subreddits():
+    subreddit_list = []
+    with open(input_list, "r") as file:
+        data = file.read()
+        subreddit_list = data.split("\n")
+    return subreddit_list
+
+def csv_setup(sub):
+    today = datetime.today().strftime('%Y-%m-%d')
+    Path("output/" + today + "/stickies").mkdir(parents=True, exist_ok=True)
+    filename = "output/" + today + "/stickies/" + sub + ".csv"
+    with open(filename, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['id', 'created', 'author', 'title', 'url', 'text'])
+    print(f'Created {filename} for /r/{sub}')
+    return filename
+
+def get_stickies(sub, output):
+    # Specify which sticky to return. 1 appears at the top (default: 1).
+    # https://praw.readthedocs.io/en/stable/code_overview/models/subreddit.html#praw.models.Subreddit.sticky
+    counter = 1
+    lastid = ''
+    while counter > 0:
+        # Depreciation Notice
+        # DeprecationWarning: Positional arguments for 'Subreddit.sticky' will no longer be supported in PRAW 8
+        # Call this function with 'number' as a keyword argument.
+        print(f'Sticky #{str(counter)}')
+        try:
+            sticky = reddit.subreddit(sub).sticky(counter)
+
+            if (sticky.id == lastid):
+                break
+            else:
+                lastid = sticky.id
+
+            row = [sticky.id, sticky.created_utc, sticky.author.name, sticky.title, sticky.url, sticky.selftext]
+            with open(output, 'a') as file:
+                writer = csv.writer(file)
+                writer.writerow(row)
+            counter += 1
+        except prawcore.exceptions.NotFound:
+            print('Not found')
+            write_log(
+                {
+                    "message": "Not Found (prawcore.exceptions.NotFound)",
+                    "severity": "WARNING",
+                    "subreddit": sub,
+                    "counter": str(counter),
+                    "output": output
+                })
+            break
+        except Exception as e:
+            print(f'Got some other error: {type(e).__name__}')
+            break
+
+    print(f'Saved {str(counter - 1)} stickies from /r/{sub} to {output}')
+
+    # if > 0 stickies saved AND if gcs == True, upload the file to GCS 
+    if (counter > 1) and (use_gcs == True):
+        write_log(
+        {
+            "message": "Uploading file to GCS",
+            "severity": "INFO",
+            "count": str(counter - 1),
+            "output": output,
+            "subreddit": sub,
+            "target-metadata": "stickies"
+        })
+        blob_name = output[7:] # slice the "output/" at the beginning of the filename to be used as the blob name in Google Cloud
+        try:
+            upload_blob(output, blob_name)
+        except Exception as e:
+            write_log(
+                {
+                    "message": "Exception when Uploading to GCS",
+                    "severity": "WARNING",
+                    "target-metadata": "stickies",
+                    "type": str(type(e)),
+                    "exception": str(e)
+                })
+    else:
+        write_log(
+        {
+            "message": "Not Uploading to GCS",
+            "severity": "INFO",
+            "subreddit": sub,
+            "target-metadata": "stickies",
+            "count": str(counter - 1),
+            "output": output
+        })
+
+def upload_blob(filename, destination_blob_name):
+    """Uploads a file to the bucket."""
+
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The contents to upload to the file
+    # contents = "these are my contents"
+
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client(project_id)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(filename)
+
+    print(
+        f"{destination_blob_name} was uploaded to {bucket_name}."
+    )
+    write_log(
+        {
+            "message": "File was uploaded",
+            "target-metadata": "stickies",
+            "severity": "INFO",
+            "destination-name": destination_blob_name,
+            "bucket-name": bucket_name
+        })
+
+def main():
+    
+    write_log(
+        {
+            "message": "** get_stickies.py | Retrieving Stickied Posts **",
+            "severity": "NOTICE",
+            "target-metadata": "stickies"
+        })
+
+    subreddit_list = get_subreddits()
+    for sub in subreddit_list:
+        output = csv_setup(sub)
+        get_stickies(sub, output)
+
+    write_log(
+        {
+            "message": "** get_stickies.py | DONE **",
+            "target-metadata": "stickies",
+            "severity": "NOTICE"
+        })
+
+if __name__ == "__main__":
+    print('** get_stickies.py | Retrieving Stickied Posts **')
+    start_time = datetime.now()
+    
+    main()
+
+    end_time = datetime.now()
+    total_time = end_time - start_time
+    print(f'Started at {start_time} and finished at {end_time}.\nTotal runtime was {total_time}')
+    print('** get_stickies.py | DONE **')
